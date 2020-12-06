8be971ff85bbb732ffca507f31e40b40677957c9
Carlos A. Leguizamón <familygw@gmail.com>
2020-12-06 13:43:44 -0300

    Initial Commit

---
+++ QB64 IDE/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  QB64 IDE
+//
+//  Created by Carlos A. Leguizamón on 06/12/2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
